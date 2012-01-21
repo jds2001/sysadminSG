159bf88f68981538ba82ee827195a9876aef248a
Jon Stanley <jonstanley@gmail.com>
2012-01-20 20:45:29 -0500

    Configure up iscsi and include the initiator stuff in common

--- appliances/sysadminSG-master/setup-iscsi.sh
+++ appliances/sysadminSG-master/setup-iscsi.sh
@@ -4,5 +4,8 @@ mkdir /iscsi-targets
 for file in $(seq 1 50); do
    dd if=/dev/zero of=/iscsi-targets/target-$file bs=1M count=10
 done
+cp /tmp/sysadminSG-master/targets.conf /etc/tgt
+chown root:root /etc/tgt/targets.conf
+chmod 600 /etc/tgt/targets.conf
 chkconfig tgtd on
 
